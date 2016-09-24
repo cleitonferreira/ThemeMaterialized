8039790dac5d31cc7d6b51dd2ebf988ee24f374f
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2016-09-24 14:09:38 -0300

    Configurando a ViewResolver com Thymeleaf

--- src/main/java/br/com/materialized/controller/MaterializedController.java
+++ src/main/java/br/com/materialized/controller/MaterializedController.java
@@ -1,8 +1,14 @@
 package br.com.materialized.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MaterializedController {
 
+	@RequestMapping("/")
+	public String principal() {
+		return "index";
+	}
+	
 }
