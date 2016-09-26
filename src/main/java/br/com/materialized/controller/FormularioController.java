6fc8f515fb401c45c03477417a5aa83020d34002
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2016-09-25 22:24:19 -0300

    Forward e Redirect

--- src/main/java/br/com/materialized/controller/FormularioController.java
+++ src/main/java/br/com/materialized/controller/FormularioController.java
@@ -3,9 +3,11 @@ package br.com.materialized.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.materialized.model.Usuario;
 
@@ -18,17 +20,20 @@ public class FormularioController {
 	}
 	
 	@RequestMapping(value = "/test/formulario", method = RequestMethod.POST)
-	public String cadastrar(@Valid Usuario usuario, BindingResult result) {
+	public String cadastrar(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if (result.hasErrors()) {
-			System.out.println(">>> Existe um erro! ");
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "/test/formulario";
 		} else {			
 			System.out.println(">>> Nome: " + usuario.getNome());
 			System.out.println(">>> E-mail: " + usuario.getEmail());
 			System.out.println(">>> Senha: " + usuario.getSenha());
 		}
 		
-		return "/test/formulario";
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Formulário salvo com sucesso!");
+		return "redirect:/test/formulario";
 		
 	}
 	
